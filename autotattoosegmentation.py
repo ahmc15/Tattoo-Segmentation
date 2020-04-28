cbff732affb3fa21ee20512d2550cb9a4eeeda70
ahmc15 <andrehmdacosta15@gmail.com>
2020-04-28 16:16:40 -0300

    added line GPU usage

--- autotattoosegmentation.py
+++ autotattoosegmentation.py
@@ -1,6 +1,17 @@
 #!/opt/conda/bin python3
+
+
 from keras_segmentation.models.unet import unet
 from keras_segmentation.models.segnet import vgg_segnet
+
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
 import os
 import math
 from PIL import Image
