d1a1b5047fe15f59c2f84dc7252be866d2c17f13
André da Costa <37544396+ahmc15@users.noreply.github.com>
2020-05-05 09:57:53 -0300

    correctec set_session

--- autotattoosegmentation.py
+++ autotattoosegmentation.py
@@ -17,7 +17,7 @@ config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
 config.log_device_placement = True  # to log device placement (on which device the operation ran)
 sess = tf.compat.v1.Session(config=config)
-set_session(sess)  # set this TensorFlow session as the default session for Keras
+#set_session(sess)  # set this TensorFlow session as the default session for Keras
 tf.compat.v1.keras.backend.set_session(sess)
 
 import os
